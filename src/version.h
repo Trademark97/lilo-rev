e973a163bd4e1c7c0732839f8eee566158f19cad
Tobias M <tmmonckton@gmail.com>
2021-07-02 02:30:43 +0000

    Update version info from 24.2 -> 24.3, part quatre (src/version.h)

--- src/version.h
+++ src/version.h
@@ -2,6 +2,7 @@
  * 
  * Copyright 2005-2007 John Coffman
  * Copyright 2009-2015 Joachim Wiedorn
+ * Copyright 2021 Tobias Monckton
  * All rights reserved.
  * 
  * Licensed under the terms contained in the file 'COPYING'
@@ -12,8 +13,8 @@
 #define VERSION_H
 
 #define VERSION_MAJOR 24
-#define VERSION_MINOR 2
+#define VERSION_MINOR 3
 #define VERSION_EDIT  ""
-#define VERSION_DATE "22-November-2015"
+#define VERSION_DATE "01-July-2021"
 
 #endif
